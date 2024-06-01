30be81666ce36064f74b038e0e8322b1f1ddedec
Jon Norberg <jon.norberg@su.se>
2024-06-01 09:10:44 +0200

    add sim to scenario

--- pluto/MS_figures.jl
+++ pluto/MS_figures.jl
@@ -21,6 +21,26 @@ function figureInstitutionalAnalysis(S::Array{Scenario})
 	
 end
 
+# ╔═╡ 88914d21-71ca-4c8e-88c2-2c1e3fd6f59a
+function Scenarios(;random=false,distribution=Uniform)
+	S=[]
+	push!(S,
+	scenario(
+		w=SED(min=0.0,max=0.3,normalize=true;random,distribution),
+		q=SED(mean=1.0,sigma=0.0,normalize=true;random)
+	)
+	)
+end
+
+# ╔═╡ 2d1dc9a6-08b5-4c36-809f-cbbf1a580795
+S=Scenarios()
+
+# ╔═╡ b43c0e48-660e-435d-8384-6c675f276c19
+phaseplot(S[1])
+
 # ╔═╡ Cell order:
+# ╠═b43c0e48-660e-435d-8384-6c675f276c19
+# ╠═2d1dc9a6-08b5-4c36-809f-cbbf1a580795
 # ╠═2b05ad03-1cf0-4ffc-87d8-4aca8e88dcdb
+# ╠═88914d21-71ca-4c8e-88c2-2c1e3fd6f59a
 # ╠═fe5ddb88-1fe3-11ef-133f-e38ab23873d9
